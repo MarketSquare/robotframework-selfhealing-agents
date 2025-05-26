d24d95658f6d05d250e0ecda1cb026c4fa9bae5b
jublankeviadee <julian.blanke@viadee.de>
2025-05-26 11:17:45 +0200

    feature: adjusted to new response type (-> str)

--- src/RobotAid/self_healing_system/agents/orchestrator_agent.py
+++ src/RobotAid/self_healing_system/agents/orchestrator_agent.py
@@ -4,10 +4,10 @@ from pydantic_ai.agent import AgentRunResult
 
 from RobotAid.utils.app_settings import AppSettings
 from RobotAid.utils.client_settings import ClientSettings
+from RobotAid.self_healing_system.schemas import PromptPayload
 from RobotAid.self_healing_system.clients.llm_client import get_model
 from RobotAid.self_healing_system.agents.locator_agent import LocatorAgent
 from RobotAid.self_healing_system.agents.prompts import PromptsOrchestrator
-from RobotAid.self_healing_system.schemas import PromptPayload, LocatorHealingResponse
 
 
 # MVP Orchestrator Agent - will be adjusted to context and when additional agents will be implemented.
@@ -22,36 +22,35 @@ class OrchestratorAgent:
     def __init__(self, app_settings: AppSettings, client_settings: ClientSettings, locator_agent: LocatorAgent, usage_limits: UsageLimits = UsageLimits(request_limit=5, total_tokens_limit=2000)) -> None:
         self.locator_agent: LocatorAgent = locator_agent
         self.usage_limits: UsageLimits = usage_limits
-        self.agent: Agent[PromptPayload, LocatorHealingResponse] = (
-            Agent[PromptPayload, LocatorHealingResponse](
+        self.agent: Agent[PromptPayload, str] = (
+            Agent[PromptPayload, str](
             model=get_model(provider=app_settings.orchestrator_agent.provider,
                             model=app_settings.orchestrator_agent.model,
                             client_settings=client_settings),
             system_prompt=PromptsOrchestrator.system_msg,
-            deps_type=PromptPayload,
-            output_type=LocatorHealingResponse
+            deps_type=PromptPayload
         ))
 
         @self.agent.tool(name="locator_heal")
-        async def locator_heal(ctx: RunContext[PromptPayload]) -> LocatorHealingResponse:
+        async def locator_heal(ctx: RunContext[PromptPayload]) -> str:
             """Invoke LocatorAgent on locator error.
 
             Args:
                 ctx (RunContext): PydanticAI tool context.
 
             Returns:
-                (LocatorHealingResponse): List of repaired locator suggestions.
+                (str): Repaired locator suggestion.
             """
             return await self.locator_agent.heal_async(ctx=ctx)
 
-    async def run_async(self, robot_ctx: dict) -> LocatorHealingResponse:
+    async def run_async(self, robot_ctx: dict) -> str:
         """Run orchestration asynchronously.
 
         Args:
             robot_ctx (dict): Contains context for the self-healing process of the LLM.
 
         Returns:
-            (LocatorHealingResponse): List of repaired locators.
+            (str): Repaired locator suggestion.
         """
         payload: PromptPayload = PromptPayload(**robot_ctx)
         response: AgentRunResult = await self.agent.run(
