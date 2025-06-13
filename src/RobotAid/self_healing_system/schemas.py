bbf62da120dc5d39a44a2d78addbe918163c01eb
Many Kasiriha <many.kasiriha@dbschenker.com>
2025-06-13 08:32:51 +0200

    feature: Add kw name, args and failed locator as separate field to PromptPayload

--- src/RobotAid/self_healing_system/schemas.py
+++ src/RobotAid/self_healing_system/schemas.py
@@ -12,3 +12,6 @@ class PromptPayload(BaseModel):
     robot_code_line: str = Field(..., description="The raw Robot keyword call that failed")
     error_msg: str = Field(..., description="The Robotframework error message")
     dom_tree: str = Field(..., description="DOM tree of website on test failure")
+    keyword_name: str = Field(..., description="Name of the Robotframework keyword that failed")
+    keyword_args: tuple = Field(..., description="Arguments of the Robotframework keyword that failed")
+    failed_locator: str = Field(..., description="Locator that failed in the Robotframework keyword")    
