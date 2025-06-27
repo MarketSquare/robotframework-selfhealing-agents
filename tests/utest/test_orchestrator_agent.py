7acfde7c048d972bc86a636324b0d3e2381711b7
Many Kasiriha <many.kasiriha@dbschenker.com>
2025-06-27 12:57:45 +0200

    test: Dix test_run_async_calls_agent_run_and_returns_output. Adjust return type of run to str etc

--- tests/utest/test_orchestrator_agent.py
+++ tests/utest/test_orchestrator_agent.py
@@ -10,7 +10,7 @@ from RobotAid.self_healing_system.schemas import PromptPayload, LocatorHealingRe
 
 
 class DummyAgentRunResult:
-    def __init__(self, output: LocatorHealingResponse) -> None:
+    def __init__(self, output: str) -> None:
         self.output = output
 
 
@@ -44,10 +44,11 @@ class StubAgent:
         prompt: str,
         deps: Any,
         usage_limits: Any,
+        model_settings: Any = None
     ) -> DummyAgentRunResult:
         self.run_calls.append((prompt, deps, usage_limits))
         return DummyAgentRunResult(
-            output=LocatorHealingResponse(suggestions=["out1", "out2", "out3"])
+            output=LocatorHealingResponse(suggestions=["out1", "out2", "out3"]).model_dump_json()
         )
 
 
@@ -112,18 +113,17 @@ def test_run_async_calls_agent_run_and_returns_output() -> None:
         locator_agent=locator_agent,
     )
 
-    robot_ctx = {"robot_code_line": "keyword call", "error_msg": "err", "dom_tree": "i1", "tried_locator_memory": ["P"]}
+    robot_ctx = {"robot_code_line": "keyword call", "error_msg": "err", "dom_tree": "i1", "tried_locator_memory": ["P"], "keyword_name": "keyword", "keyword_args": ("arg1", "arg2"), "failed_locator": "locator"}
     result = asyncio.new_event_loop().run_until_complete(
         orch.run_async(robot_ctx=robot_ctx)
     )
 
-    assert isinstance(result, LocatorHealingResponse)
-    assert result == LocatorHealingResponse(suggestions=["out1", "out2", "out3"])
+    assert isinstance(result, str)
+    assert LocatorHealingResponse.model_validate_json(result) == LocatorHealingResponse(suggestions=["out1", "out2", "out3"])
 
     assert len(orch.agent.run_calls) == 1   # type: ignore
     prompt, deps, usage = orch.agent.run_calls[0]   # type: ignore
-    assert prompt == ("Please call the tool 'get_healed_locators'. Only respond with the message the tool gave you, do "
-                      "not add any additional information in any case.")
+    assert prompt == ("Call the 'get_healed_locators' tool for the broken locator: `locator`.\n")
     assert isinstance(deps, PromptPayload)
     assert deps.error_msg == "err"
     assert deps.dom_tree == "i1"
