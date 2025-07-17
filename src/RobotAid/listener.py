802addb4d41df8a976e5f70753ebda149a373364
jublankeviadee <julian.blanke@viadee.de>
2025-07-17 10:35:02 +0200

    misc: added comment; rmved debug statement

--- src/RobotAid/listener.py
+++ src/RobotAid/listener.py
@@ -114,9 +114,8 @@ class RobotAid(ListenerV3):
             # This would store information for post-execution healing
 
     def close(self):
-        if self.report_info:
+        if self.report_info:    # close() method is called twice, resulting in removal of previous reports
             ReportGenerator().generate_reports(report_info=self.report_info)
-        debug=True
 
     def _start_self_healing(self, result: result.Keyword) -> None:
         """Starts the self-healing process via pydanticAI agentic system. Sets class attributes for further processing."""
