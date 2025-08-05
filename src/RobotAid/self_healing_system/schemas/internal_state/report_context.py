d0dabeaeff22c6957cf0c5a0737d803513118f8a
jublankeviadee <julian.blanke@viadee.de>
2025-08-05 11:04:44 +0200

    misc: init

---
+++ src/RobotAid/self_healing_system/schemas/internal_state/report_context.py
@@ -0,0 +1,11 @@
+from typing import List
+from pathlib import Path
+from pydantic import BaseModel, Field
+
+from RobotAid.self_healing_system.schemas.internal_state.report_data import ReportData
+
+
+class ReportContext(BaseModel):
+
+    report_info: List[ReportData] = Field(..., description="Report info containing data about healed locators.")
+    external_resource_paths: List[Path] = Field(default_factory=list, description="Paths of external resource files.")
